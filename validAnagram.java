89e32245b3320cc939a5e42b24f505e98e133164
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-12 22:44:06 +0530

    LeetCode 242

---
+++ validAnagram.java
@@ -0,0 +1,39 @@
+public class validAnagram {
+
+    public static boolean isAnagram(String s, String t) {
+
+        int n1 = s.length();
+        int n2 = t.length();
+
+        if (n1 != n2) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < n1; i++) {
+            count[s.charAt(i) - 'a']++;
+            count[t.charAt(i) - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        if (isAnagram(s, t)) {
+            System.out.println("Strings are Anagrams");
+        } else {
+            System.out.println("Strings are not Anagrams");
+        }
+    }
+}
