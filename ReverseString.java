3d5b05a6d827eaf191b81031f5ea603a391dd2bd
Deepanshi_Mahajan <deepanshimahajan214@gmail.com>
2026-06-12 23:05:26 +0530

    LeetCode 151

---
+++ ReverseString.java
@@ -0,0 +1,29 @@
+public class ReverseString {
+    
+    public String reverseWords(String s){
+        s = s.trim();
+
+        String[] words = s.split("\\s+");
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]);
+
+            if(i!=0){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+
+    }
+
+    public static void main(String[] args) {
+        String s = "the sky is blue";
+
+        ReverseString obj = new ReverseString();
+
+        String result = obj.reverseWords(s);
+
+        System.out.println(result);
+    }
+}
